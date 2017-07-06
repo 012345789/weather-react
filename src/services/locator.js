63a71450cc310d1b71e3ad27a55f2e05323c099f
012345789 <holyduper@gmail.com>
2017-07-06 13:47:55 -0700

    Add error handling, UI refinements, and better structured code.

--- src/services/locator.js
+++ src/services/locator.js
@@ -2,7 +2,6 @@ import fetch from 'isomorphic-fetch';
 import _ from 'underscore';
 
 // Returns an estimate of the user's location based on visiting IP address
-// Requires ad-blockers to be disabled
 export default function locateUser() {
   const ipLocatorUrl = 'http://ip-api.com/json/';
   return fetch(ipLocatorUrl)
